5b703d595da6d0557212c834f14884382414ff28
AP <appanchal.ca@gmail.com>
2025-08-19 12:30:24 -0400

    changes in vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      input: path.resolve(__dirname, "index.html"),
+    },
+  },
 });
